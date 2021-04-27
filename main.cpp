6f4094ea1e6bc24a6cb5145562cdba2ddd7bf6e0
samsongalaxy <samsongalaxy@gmail.com>
2021-04-26 20:54:45 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -14,26 +14,26 @@ int main(int argc, char **argv){
   cout << "Beginning test...\n";
   Knapsack k;
   k.openFile(argv[2]);
-  //cout << "algorithm_flag should be " << argv[3] << "\n";
+  cout << "algorithm_flag should be " << argv[3] << "\n";
   int algorithm_flag = stoi(argv[3]);
-  //cout << "algorithm_flag is " << algorithm_flag << "\n";
+  cout << "algorithm_flag is " << algorithm_flag << "\n";
   ifstream fin;
   fin.open(argv[1]);
   while(!fin.eof()){
     string temp;
     getline(fin, temp, ' ');
-    //cout << "n should be " << temp << "\n";
+    cout << "n should be " << temp << "\n";
     int n = stoi(temp);
     getline(fin, temp, '\n');
     int c = stoi(temp);
-    //cout << "n is " << n << " and c is " << c << "\n";
+    cout << "n is " << n << " and c is " << c << "\n";
     k.setCapacity(n, c);
     for(int i = 0; i < n; i++){
       getline(fin, temp, ' ');
       double w = stoi(temp);
       getline(fin, temp, '\n');
       double p = stoi(temp);
-      //cout << "w is " << w << " and p is " << p << "\n";
+      cout << "w is " << w << " and p is " << p << "\n";
       k.addItem(w, p, i+1);
     }
     if(algorithm_flag == 0) k.greedy1(1);
