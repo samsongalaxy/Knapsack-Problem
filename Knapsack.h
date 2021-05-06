b2a51975c2049cf709ad31f35443b4c6b9cb3e1b
samsongalaxy <samsongalaxy@gmail.com>
2021-05-06 17:14:18 -0400

    for remote

--- Knapsack.h
+++ Knapsack.h
@@ -19,6 +19,7 @@ class Knapsack{
     void greedy2(int will_print);
     void backtracking();
     void bt_helper(int i, int max, int p);
+    void dynamicprogramming(); //function for program 4
     bool solution;
     int num_of_items, capacity, profit, weight, location, maxprofit;
     double profit_weight_ratio;
